ed93f461e677e0b328923583eada341765cb6198
Kadir Kul <56093519+kkul40@users.noreply.github.com>
2024-12-30 00:41:39 -0500

    Update

--- Assets/_Project/Script/CharacterCustomization/CharacterCustomizationDataSo.cs
+++ Assets/_Project/Script/CharacterCustomization/CharacterCustomizationDataSo.cs
@@ -9,14 +9,14 @@ namespace CharacterCustomization
         [System.Serializable]
         public class ItemGroup
         {
-            public GameObject m_BaseBody;
-            public CustomizationItemAsset[] m_Heads;
-            public CustomizationItemAsset[] m_Hairstyles;
-            public CustomizationItemAsset[] m_HeadAccessories;
-            public CustomizationItemAsset[] m_Tops;
-            public CustomizationItemAsset[] m_Bottoms;
-            public CustomizationItemAsset[] m_Shoes;
-            public CustomizationItemAsset[] m_Outfits;
+            // public GameObject m_BaseBody;
+            // public CustomizationItemAsset[] m_Heads;
+            // public CustomizationItemAsset[] m_Hairstyles;
+            // public CustomizationItemAsset[] m_HeadAccessories;
+            // public CustomizationItemAsset[] m_Tops;
+            // public CustomizationItemAsset[] m_Bottoms;
+            // public CustomizationItemAsset[] m_Shoes;
+            // public CustomizationItemAsset[] m_Outfits;
         }
 
         [SerializeField] private float m_FakeLoadTime = 1f;
