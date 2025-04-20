d55215d5e2bc616b6ef52851a6dac46741302fd0
Kadir Kul <56093519+kkul40@users.noreply.github.com>
2025-04-20 13:15:04 -0400

    +

--- Assets/_Project/Script/_Refactored/System/Character/NPC/NpcAnimationSo.cs
+++ Assets/_Project/Script/_Refactored/System/Character/NPC/NpcAnimationSo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace System.Character.NPC
@@ -7,28 +6,27 @@ namespace System.Character.NPC
     [CreateAssetMenu(fileName = "new Npc Animation Set", menuName = "Data/Npc Animation Set")]
     public class NpcAnimationSo : ScriptableObject
     {
+        [Serializable]
+        public class ActionAnimation
+        {
+            public AvatarMask Mask;
+            public AnimationClip IdleClip;
+            public AnimationClip ActionClip;
+        }
+        
+        
         public float animationDuration;
 
-        [Title("Animations")] 
         public AnimationClip Idle;
         public AnimationClip Walk;
         public AnimationClip Sit;
         public List<AnimationClip> LeanOnWall;
         public List<AnimationClip> Dance;
 
-        [Title("Action Animations")] 
         public ActionAnimation Drink;
         public ActionAnimation Fighh;
 
-        [Title("Debug")] 
         public AnimationClip DebugAnim;
     }
-
-    [Serializable]
-    public class ActionAnimation
-    {
-        public AvatarMask Mask;
-        public AnimationClip IdleClip;
-        public AnimationClip ActionClip;
-    }
+ 
 }
